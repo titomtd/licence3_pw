9d0ec798ffd36eebf372e4ee97e73ea7bd8f948f
Thomas Duchemin <th0m4s35160@gmail.com>
2021-01-05 18:17:06 +0100

    Feature for adding my account profil page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ResetPasswordType;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserController extends AbstractController
 {
@@ -44,6 +50,62 @@ class UserController extends AbstractController
     {
         return $this->render('user/show.html.twig', [
             'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/my-account/dashboard", name="my_account_dashboard")
+     */
+    public function dashboard(?UserInterface $user)
+    {
+        return $this->render('myaccount/dashboard.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/my-account/update-profile", name="my_account_update_profile")
+     */
+    public function updateProfile(Request $request, ObjectManager $manager, ?UserInterface $user)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('my_account_update_profile');
+        }
+        return $this->render('myaccount/update_profile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/my-account/comments", name="my_account_comments")
+     */
+    public function allComments(?UserInterface $user)
+    {
+        return $this->render('myaccount/comments.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/my-account/update-password", name="my_account_update_password")
+     */
+    public function updatePassword(Request $request, ObjectManager $manager, ?UserInterface $user, UserPasswordEncoderInterface $encoder)
+    {
+       /* $form = $this->createForm(ResetPasswordType::class, $user);
+
+        $form->handleRequest($request);*/
+
+        return $this->render('myaccount/update_password.html.twig', [
+            /*'user' => $user,
+            'form' => $form->createView()*/
         ]);
     }
 }
